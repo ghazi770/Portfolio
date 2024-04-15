cb4ad37345cae4dc8f42ca8e1ed65e296d5067fc
Ghazi shamroz Khan <khanghazi770@gmail.com>
2024-04-15 22:04:45 +0500

    Registeration changes

--- cypress/e2e/PractiseAutomationCases/Registration.cy.js
+++ cypress/e2e/PractiseAutomationCases/Registration.cy.js
@@ -6,9 +6,64 @@ describe('Registeration', () =>
 beforeEach(() => {
 cy.visit("https://practice.automationtesting.in/my-account/") }) 
 
+afterEach(() => {
+cy.reload() }) 
+
   const ln=new Registration();
 
-  // Case: #005
+
+  // Case: #002
+
+it.only('Registeration with Empty Email ', () => {
+        
+  cy.fixture("practiceautomationtesting").then((data)=>{
+
+  ln.settxtuserEmail(data.Incorrect_email)
+  ln.set_Password(data.Incorrect_passwords)
+  ln.regis_button()
+  cy.wait(50)
+  cy.get('.woocommerce-error > li').
+   should('have.text','Error: Please enter an account password.');
+              
+  })
+            
+  })
+ 
+
+
+
+ // Case: #003
+
+ it('Registeration with Empty Email ', () => {
+        
+  cy.fixture("practiceautomationtesting").then((data)=>{
+  ln.settxtuserEmail(data.set_Password)
+  ln.regis_button()
+  cy.wait(50)
+  cy.get('.woocommerce-error > li').
+   should('have.text','Error: Please enter an account password.');
+              
+  })
+            
+  })
+
+  // Case: #004
+
+  it('Registeration with Empty Password ', () => {
+        
+    cy.fixture("practiceautomationtesting").then((data)=>{
+    ln.settxtuserEmail(data.Incorrect_email)
+    ln.regis_button()
+    cy.wait(50)
+    cy.get('.woocommerce-error > li').
+     should('have.text','Error: Please enter an account password.');
+                
+    })
+              
+    })
+
+
+ // Case: #005 
 
 it ('Registeration with Empty Email_Id & Password ', () => {
 
