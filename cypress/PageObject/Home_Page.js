ea9625ac72b69ed08393a41e26970ad1cd63b79d
Ghazi shamroz Khan <khanghazi770@gmail.com>
2024-06-02 21:36:51 +0500

    My Home Next addition

--- cypress/PageObject/Home_Page.js
+++ cypress/PageObject/Home_Page.js
@@ -1,20 +1,52 @@
 class HomePage{
 
+text_coup="input[name='apply_coupon']";
+fie_coup="input[id='coupon_code']";
+x_icon="a[title='Remove this item']";
+
 link_shop(){
 cy.contains('Home').click()
 
 }   
 
-Click_link(){
+Click_link()
+{
 cy.contains('Selenium Ruby').click()
     
 }   
     
-Review_section(){
+Review_section()
+{
 cy.contains('Reviews').click()
         
-    }   
-    
+}   
+Description_section()
+{
+cy.contains("Product Description")    
+}    
 
-   }
-   export default HomePage;
+Add_Basket()
+{
+cy.contains('Add to basket').click()
+
+}
+
+View_basket()
+{
+cy.contains('View Basket').click()
+
+}
+
+apply_coupon(){
+cy.get(this.fie_coup).type("0900-12")
+cy.get(this.text_coup).click()    
+}
+
+remove_item()
+{
+cy.get(this.x_icon).click()    
+
+}
+}
+
+export default HomePage;
--- cypress/e2e/Home.cy.js
+++ cypress/e2e/Home.cy.js
@@ -15,13 +15,51 @@ describe('Home', () =>
 
 // Case: #003 & #005
 
-it ('Home Page- Images in arrival should be navigate & See Review Section', () => {
+it ('Home Page', () => {
 
 home.link_shop()
 cy.wait(50)   
 home.Click_link()
 cy.url().should('eq', 'https://practice.automationtesting.in/product/selenium-ruby/');
+
+// Case: #004 ['Home Page - Arrival Image-Description '] 
+home.Description_section()
+
+// Case: #005 ['Home Page - Arrival Image-Review section '] 
 home.Review_section()
+
+// Case: #006 ['Home Page - Arrival Image-Add to Basket ']
+
+home.Add_Basket()
+cy.get('span[class="cartcontents"]').should('have.text','1 item')
+
+// Case: #007 Currently not possible
+
+// Case: #008 ['Home Page - Arrival Image-Add to Basket items']
+
+home.View_basket()
+cy.url().should('eq', 'https://practice.automationtesting.in/basket/');
+
+// Increase quantiy
+
+//const newQuantity = parsedQuantity + 1;
+
+// Set the new quantity
+//cy.get('@quantityInput').clear().type(newQuantity);
+
+
+// Case: #009 ['Home Page - Arrival Image-Add to Basket items-coupon']
+
+home.apply_coupon()
+
+//cy.get('//*[@id="page-34"]/div/div[1]/ul/li').should('have.text','Coupon "0900-12" does not exist!')
+
+// Case: #010- Currently no way to create coupan
+
+// Case: #011 ['Home Page - Arrival Image-Add to Bask-Remove item']
+
+home.remove_item()
+cy.get('.cart-empty').should('have.text', '\n\tYour basket is currently empty.')
 
 })  
 
